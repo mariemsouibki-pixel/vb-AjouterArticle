c8311622dff4ed4fcae17f55465572e4a1ab3047
souibki mariem <mariem.souibki@isgb.ucar.tn>
2025-11-26 22:17:53 +0100

    Add files via upload

---
+++ Articles.vb
@@ -0,0 +1,22 @@
+﻿Module Articles
+    Public Structure art
+        Dim code As Integer
+        Dim designation As String
+        Dim prix As Integer
+        Dim quantite As Integer
+    End Structure
+    Public listeArticle As New List(Of art)
+    Public Sub ajouterarticle(a As art)
+        listeArticle.Add(a)
+
+
+    End Sub
+
+
+
+
+
+
+
+
+End Module
